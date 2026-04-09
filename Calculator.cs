05ec9a88a2b67d8489cd90e5b4cf9740d4aad49f
Roy <r.widyadhana@students.amikom.ac.id>
2026-04-09 11:17:34 +0700

    Tugas 4 Melengkapi fungsi main

--- Calculator.cs
+++ Calculator.cs
@@ -5,6 +5,8 @@ class Calculator
 		int a = 10;
 		int b = 6;
 		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, penjumlahan(a, b));
+		
 	}
 	
 	static int penjumlahan(int a, int b)
